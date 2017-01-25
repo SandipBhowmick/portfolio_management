a110a08f504bb8c97f7b20a1f5852b5bdf42c0ce
SandipBhowmick <bhowmick.sandip@gmail.com>
2017-01-25 21:28:14 +0530

    Add user controller update 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+
+layout "admin"
+
+def index
+	@users = User.order("user_name asc").all
+end
+
+def show
+	@user = User.find(params[:id])
+end
+
+def new
+
+end
+
+def create
+
+end
+
+def edit
+
+end
+
+def update
+
+end
+
+def destroy
+
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :shares do
-  		collection do
-    		match 'search' => 'shares#search', via: [:get, :post], as: :search
-  		end
-	end
-
-	# resources :equitys , only:[:index]
- 
+	resources :users do
+	  resources :shares do
+	  		collection do
+	    		match 'search' => 'shares#search', via: [:get, :post], as: :search
+	  		end
+		end
+	end	
  end
